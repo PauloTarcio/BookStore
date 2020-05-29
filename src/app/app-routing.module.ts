d125118065251948f200b8531fc79afe002e7606
Paulo Tarcio <pvtarcio@gmail.com>
2020-05-29 11:27:52 -0400

    page not found criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -7,6 +8,8 @@ const routes: Routes = [
   { path: 'books', loadChildren: () => import('./books/books.module').then((m) => m.BooksModule) },
   { path: 'authors',  loadChildren: () => import('./authors/authors.module').then((m) => m.AuthorsModule) },
   { path: 'genres',  loadChildren: () => import('./genres/genres.module').then((m) => m.GenresModule) },
+
+  { path: '**', component: PageNotFoundComponent  },
   ];
 
 @NgModule({
